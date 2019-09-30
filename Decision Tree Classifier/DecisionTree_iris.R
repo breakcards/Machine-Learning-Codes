827482820a1fb2991ccda7c0ef53795df93f4759
nikhilshinde095 <53527345+nikhilshinde095@users.noreply.github.com>
2019-09-30 22:25:44 +0530

    Add files via upload

---
+++ Decision Tree Classifier/DecisionTree_iris.R
@@ -0,0 +1,21 @@
+#=======================================================================================================
+# Program For Decision Tree Using iris Data:- 
+#=======================================================================================================
+library(rpart) #library for Decision tree for generation of parts
+Dataset=iris  
+shuff=Dataset[sample(1:nrow(Dataset)),] # Shuffling the given dataset using sample command
+trainData=shuff[1:125,] # divide that data into test & train 
+testData=shuff[126:150,]
+tree=rpart(Species ~ Sepal.Length+ Sepal.Width+Petal.Length+Petal.Width,trainData,method="class") # for generating tree
+plot(tree)                            #plotting the tree 
+text(tree, use.n = TRUE)  
+predicted=round(predict(tree,testData)) #rounding the values 
+Npredict=NULL
+for (i in 1:nrow(predicted)) 
+{
+  prediction=as.numeric(which.max(predicted[i,]))
+  Npredict=c(Npredict,prediction)
+}
+compare=(Npredict==as.numeric(testData$Species))               
+accuracy=sum(compare)/length(Npredict)*100                      # accuracy
+print(accuracy)                  
